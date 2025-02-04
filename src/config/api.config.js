5d44e5c6caa45c3f0056ce3b608f25bd4813dc2e
Eloura74 <faber.quentin@gmail.com>
2025-02-04 23:13:28 +0100

    .env secure

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   newsapi: {
-    key: "48da6f2fcbda49b8932b74c21d9b50c7",
+    key: import.meta.env.VITE_NEWSAPI_KEY,
     maxRequestsPerDay: 90,
     requestInterval: 3600000 * 4, // 4 heures entre chaque requête pour ne pas dépasser la limite
   },
